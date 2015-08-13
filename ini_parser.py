67abd30d854987bc640511edb4c0093b473ae6a9
Ilya Katsnelson <katsnelson@gmail.com>
2015-08-12 22:11:29 -0700

    Some fixes. Add more methods to read data and read data status.

--- ini_parser.py
+++ ini_parser.py
@@ -9,7 +9,8 @@ Created on Sat Aug 08 15:58:20 2015
 
  Syntax:
  1. String starting from '//', '#', or ';' are concidered comments.
- 2. 
+ 2. Sections are denoted by []
+ 3. The data is store in 'key'='value' pairs
 
 """
 import sys, os, re
@@ -34,36 +35,38 @@ class ini_data:
 
     # Global constants
     _comment_key = "_comment"
+    _comment_head = '#'
+
     # Dictionary of all INI entries
     _data = None
-    
+
     def __init__(self, file_name=None):
         self._data = dict()
         if (None != file_name):
             self.parse(file_name, False)
-        
+
     def __str__(self):
         output = ""
         if (len(self._data) != 0):
             # print the data in proper INI format
             ini_int_data = self._data
-            
+
             # print the global comment
             if (ini_int_data.has_key(self._comment_key) == True):
                 for comment in ini_int_data[self._comment_key]:
-                    output += ("# %s\n" % comment)
+                    output += ("%s %s\n" % (self._comment_head, comment))
                 output += "\n"
 
             for section in ini_int_data:
                 if (section != self._comment_key):
                     output += ("[%s]\n" % (section))
                     section_dict = ini_int_data[section]
-                    
+
                     # print the section comment
                     if (section_dict.has_key(self._comment_key) == True):
                         for comment in section_dict[self._comment_key]:
-                            output += ("# %s\n" % comment)
-                        
+                            output += ("%s %s\n" % (self._comment_head, comment))
+
                     for key in section_dict:
                         if (key != self._comment_key):
                             output += ("%s=%s\n" % (key, section_dict[key]))
@@ -94,7 +97,7 @@ class ini_data:
                 for ini_line in ini_file:
                     # skip the empty lines
                     if (len(ini_line) == 0):
-                        continue;
+                        continue
 
                     ini_line = ini_line.strip()
 
@@ -104,7 +107,7 @@ class ini_data:
                         if (ini_int_data.has_key(self._comment_key) == False):
                             ini_int_data[self._comment_key] = []
                         ini_int_data[self._comment_key].append(matchObj.group(2));
-                            
+
                         continue
 
                     # Find if the line is a section in []
@@ -121,9 +124,9 @@ class ini_data:
 
                             ini_int_data[matchObj.group(1)] = dict()
                             ini_int_data = ini_int_data[matchObj.group(1)]
-                        
-                        continue;
-                        
+
+                        continue
+
                     # Find if the line is some data. Error if not in a section.
                     matchObj = re.match(r'^(.+)=(.*)', ini_line);
                     if (None != matchObj):
@@ -135,17 +138,17 @@ class ini_data:
 
         except KeyError, e:
             print ("Error: %s" % e)
-            status = False;
+            status = False
         except IOError, e:
-            print ("IOError: %s\n" % sys.exc_info()[1]);
-            traceback.print_exc(file = sys.stdout);
-            status = False;
+            print ("IOError: %s\n" % sys.exc_info()[1])
+            traceback.print_exc(file = sys.stdout)
+            status = False
         except:
-            print ("Unexpected error: %s\n" % sys.exc_info()[1]);
-            traceback.print_exc(file = sys.stdout);
-            status = False;
+            print ("Unexpected error: %s\n" % sys.exc_info()[1])
+            traceback.print_exc(file = sys.stdout)
+            status = False
 
-        return status;
+        return status
 
     def add_comment(self, comment, section=None):
         '''
@@ -156,18 +159,18 @@ class ini_data:
         if (section != None):
             if (ini_int_data.has_key(section) == False):
                 ini_int_data[section] = dict()
-                
+
             ini_int_data = ini_int_data[section]
-            
+
         # Find if there are any comment sections
         if (ini_int_data.has_key(self._comment_key) == False):
             ini_int_data[self._comment_key] = []
-            
+
         comment_lines = comment.splitlines();
         comment_list = ini_int_data[self._comment_key]
         for line in comment_lines:
             comment_list.append(line)
-        
+
         ini_int_data[self._comment_key] = comment_list
 
         return True
@@ -180,13 +183,13 @@ class ini_data:
         ini_int_data = self._data
         if (ini_int_data.has_key(section) == False):
             ini_int_data[section] = dict()
-            
+
         # Exit if we only want to add a section
         if (key == None):
             return True
-            
+
         ini_int_data = ini_int_data[section]
-            
+
         # Find duplicates
         if (ini_int_data.has_key(key) == True):
             raise iniException("Error: key '%s' already exist." % key)
@@ -196,36 +199,60 @@ class ini_data:
 
         return True
 
-    def get_data(self, section, key):
+    def get_data(self, section, key=None):
         '''
         Get value for the specified key from key+value pair to the specified section.
-        Return value. Throw iniException in case of the error
+        Get the list of key+value tuples from the specified section if key is None.
+        Return value or list. Throw iniException in case of the error
         '''
         ini_int_data = self._data
         if (ini_int_data.has_key(section) == False):
             raise iniException ("Error: section '%s' doesn't exist." % section)
-            
-        ini_int_data = ini_int_data[section]
-            
-        # Find duplicates
-        if (ini_int_data.has_key(key) == False):
-            raise iniException ("Error: key '%s' doesn't exist." % key)
 
-        # Find if there are any comment sections
-        return (ini_int_data[key])
+        ini_int_data = ini_int_data[section]
+
+        # Find the values
+        if (key != None):
+            # return the value
+            if (ini_int_data.has_key(key) == False):
+                raise iniException ("Error: key '%s' doesn't exist." % key)
+    
+            # Find if there are any comment sections
+            return (ini_int_data[key])
+        else:
+            # return the list of key+values
+            results = []
+            for key in ini_int_data:
+                if (key != self._comment_key):
+                    results.append((key, ini_int_data[key]))
+            return results
+
+    def is_data(self, section, key):
+        '''
+        Return true if the specified key exist in the specified section
+        '''
+        ini_int_data = self._data
+        if (ini_int_data.has_key(section) == False):
+            return False
+
+        # Find the values
+        if (ini_int_data[section].has_key(key) == False):
+            return False
+
+        return True
 
 ###############################################################################
 def main():
     print("Autotesting of the ini parcer.\n")
-    
+
     options = ini_data('C:\Users\ilya\mercurial.ini')
     print options
     print ("%s" % '-'*40)
-    
+
     opts = ini_data();
     opts.add_comment("Simple testing code.\nVersion 1")
-    opts.add_data('header','version',1);
-    
+    opts.add_data('header','version',1)
+
     print("Check the error reporting.")
     print("Adding duplicate value")
     try:
@@ -233,7 +260,7 @@ def main():
     except iniException, e:
         print ("Error: %s" % e)
         print("Error adding duplicate!")
-        
+
     # More data to actually create something intersting
     opts.add_data('system info')
     opts.add_comment('more information about the system', 'system info')
@@ -244,6 +271,13 @@ def main():
 
     print("Get the values back")
     print("Version: %s\n" % opts.get_data('header', 'version'))
+
+    print("Get the values back 2")
+    for b in opts.get_data('system info'):
+        key, val = b
+        print("%s-%s" % (key, val))
+    
+    print ("%s" % '-'*40)
     print opts
 
 ###############################################################################
